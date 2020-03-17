71b1d1cb7b72433a5d5a2655388f92838aeb5205
EvanGalea2 <evanjamesgalea@gmail.com>
2020-03-16 23:16:54 -0400

    updated object references, defined board structure

--- Board.py
+++ Board.py
@@ -1,11 +1,35 @@
+from array import *
 
-def checkRow(xPos):
-    print "checking row"
-def checkColumn(xPos):
-    print "checking column"
-def checkSquare(xPos):
-    print "checking square"
-def checkAll():
-    checkRow()
-    checkColumn()
-    checkSquare()
+class Board:
+    # data = ""
+
+    def __init__(self): #currently just 9x9 board
+        self.data = [9]
+        for x in self.data:
+            x = [9]
+
+    def testBoard(self): #this method tests the 2d array
+        for x in range(0, 8):
+            for y in range(0, 8):
+                a = self.data[x]
+                a[y] = y
+    def checkRow(self, yPos):
+        print "checking row"
+    def checkColumn(self, xPos):
+        print "checking column"
+    def checkSquare(self, xPos):
+        print "checking square"
+
+    # def checkAll():
+    #     checkRow()
+    #     checkColumn()
+    #     checkSquare()
+
+    def printBoard(self):
+        for x in range(0, 8):
+            for y in range(0, 8):
+                print(x + " " + y)
+b = Board()
+b.testBoard()
+print("test")
+b.printBoard()
